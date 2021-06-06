b2ae6db991abf948a3093a95c3dd196998b80fc3
AMIT SHARMA <amitsharma@MacBook-Pro.local>
2021-06-07 01:55:38 +0530

    Starred friend implementation

--- src/components/friends/FriendListComponent.js
+++ src/components/friends/FriendListComponent.js
@@ -1,24 +1,26 @@
-import React, { useState } from 'react';
-import { ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText, Row, Col, Input } from 'reactstrap';
-import { Star, Trash } from 'react-bootstrap-icons';
+import React, { useEffect, useState } from 'react';
+import { ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText, Row, Col, Input, Button } from 'reactstrap';
+import { Star, Trash, StarFill } from 'react-bootstrap-icons';
 
 export const FriendListComponent = () => {
   // TODO: Use i18
 
-  //TODO Mock data using mirage
+  const generatRandomId = () => Math.random().toString(36).substr(2, 9);
+
+  //TODO Mock data using mirage server
   let friendList = [
     {
-      id: 1,
+      id: generatRandomId(),
       name: 'Amit Sharma',
       isStarred: true,
     },
     {
-      id: 2,
+      id: generatRandomId(),
       name: 'Shivangi Sharma',
-      isStarred: true,
+      isStarred: false,
     },
     {
-      id: 3,
+      id: generatRandomId(),
       name: 'Akash Singh',
       isStarred: true,
     },
@@ -26,22 +28,48 @@ export const FriendListComponent = () => {
 
   const [friendName, setFriendName] = useState('');
   const [data, setFriendsData] = useState(friendList);
+  const [isStarredSort, setIsStarredSort] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
   const isYourFriendString = 'is Your Friend';
+
+  useEffect(() => {
+    const sorted = [...data]?.sort((a, b) => {
+      return isStarredSort ? b['isStarred'] - a['isStarred'] : a['isStarred'] - b['isStarred'];
+    });
+    setFriendsData(sorted);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isStarredSort]);
 
   const addFriend = target => {
     if (target.charCode === 13) {
       const friendObject = {
         name: friendName,
         isStarred: false,
-        id: 4,
+        id: generatRandomId(),
       };
-      let obj = [...data];
-      obj.unshift(friendObject);
-      setFriendsData(obj);
+
+      let friendData = [...data];
+      friendData.unshift(friendObject);
+      setFriendsData(friendData);
       setFriendName('');
     }
   };
 
+  const updateStarredFriend = id => {
+    const friendList = [...data];
+    let index = friendList?.findIndex(obj => obj.id === id);
+    friendList[index].isStarred = !friendList[index].isStarred;
+    setFriendsData(friendList);
+  };
+
+  const deleteFriend = id => {
+    // setIsLoading(true);
+    const filteredList = data?.filter(item => item.id !== id);
+    // setTimeout(() => {
+    setFriendsData(filteredList);
+    //   setIsLoading(false);
+    // }, 2000);
+  };
   return (
     <>
       <Row>
@@ -52,13 +80,24 @@ export const FriendListComponent = () => {
             </ListGroupItem>
 
             <ListGroupItem>
-              <Input
-                type="text"
-                placeholder="Enter your friend's name"
-                onKeyPress={addFriend}
-                value={friendName}
-                onChange={e => setFriendName(e.target.value)}
-              />
+              <Row>
+                <Col sm="10">
+                  <Input
+                    type="text"
+                    placeholder="Enter your friend's name"
+                    onKeyPress={addFriend}
+                    value={friendName}
+                    onChange={e => setFriendName(e.target.value)}
+                  />
+                </Col>
+                <Col sm="1">
+                  {isStarredSort ? (
+                    <StarFill onClick={() => setIsStarredSort(!isStarredSort)} />
+                  ) : (
+                    <Star onClick={() => setIsStarredSort(!isStarredSort)} />
+                  )}
+                </Col>
+              </Row>
             </ListGroupItem>
 
             {data?.map(({ name, id, isStarred }) => {
@@ -70,14 +109,16 @@ export const FriendListComponent = () => {
                       <ListGroupItemText>{isYourFriendString}</ListGroupItemText>
                     </Col>
                     <Col sm="1">
-                      <Star />
+                      {isStarred ? (
+                        <StarFill onClick={() => updateStarredFriend(id)} />
+                      ) : (
+                        <Star onClick={() => updateStarredFriend(id)} />
+                      )}
                     </Col>
-                    <Col
-                      sm="2"
-                      onClick={() => {
-                        console.log('hii');
-                      }}>
-                      <Trash />
+                    <Col sm="2">
+                      <Button variant="light">
+                        <Trash onClick={() => deleteFriend(id)} />
+                      </Button>
                     </Col>
                   </Row>
                 </ListGroupItem>
